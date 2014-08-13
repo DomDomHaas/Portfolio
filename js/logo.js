5a2c3bb0d185987c7d9302b55aa14ac7b5aa84ee
DomdomHaas <shaodiese@gmail.com>
2014-08-13 20:57:48 +0200

    finished circle menu; anchor links are still jumping no transition yet

--- js/logo.js
+++ js/logo.js
@@ -1,19 +1,35 @@
 var items = document.querySelectorAll('.circle a');
 
+
+/* having 8 items */ 
 for(var i = 0, l = items.length; i < l; i++) {
-  items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-  
-  items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+    
+    items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";  
+    items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+    
+    // grouping
+    if (i == 2 && items[i].style.top == "50%"){
+        items[i].style.top = "55%";
+    }
+    if (i == 4 && items[i].style.left == "50%"){
+        items[i].style.left = "55%";
+    }
+    if (i == 6 && items[i].style.top == "50%"){
+        items[i].style.top = "55%";
+    }
+    
 }
 
-document.querySelector('.menu-button').onclick = function(e) {
+document.querySelector('.logoimg').onclick = function(e) {
    e.preventDefault(); document.querySelector('.circle').classList.toggle('open');
 }
 
-
+/*
 $('.logo > img').hover(
     function(){ $(this).addClass('active') },
     function(){ $(this).removeClass('active') }
 )
 
 $('.logo > img').click(function(){$(this).toggleClass('active')});
+
+*/
