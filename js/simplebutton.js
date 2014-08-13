f6f00743187eed0a4c2180086647ceb84bd6409a
DomdomHaas <shaodiese@gmail.com>
2014-08-13 02:17:06 +0200

    first draft finished

--- js/simplebutton.js
+++ js/simplebutton.js
@@ -1,8 +1,1 @@
-/*
-$('.morph-button').hover(
-    function(){ $(this).addClass('active open') },
-    function(){ $(this).removeClass('active open') }
-)
-*/
-
 $('.morph-button').click(function(){$(this).toggleClass('active open')});
